f427dcd98151a0e7902a7deb5805eed2cd94b9f3
subroh0508 <subroh.0508@gmail.com>
2019-05-06 16:58:39 +0900

    [add]Add buildSrc directory

---
+++ buildSrc/src/main/java/Dep.kt
@@ -0,0 +1,5 @@
+@Suppress("unused")
+object Dep {
+    const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val kotlinxHtml = "org.jetbrains.kotlinx:kotlinx-html-js:${Versions.kotlinxHtml}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,5 @@
+@Suppress("unused")
+object Versions {
+    const val kotlin = "1.3.31"
+    const val kotlinxHtml = "0.6.12"
+}
