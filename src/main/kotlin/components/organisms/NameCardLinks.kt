88474c637fff9f09ae462a7bdf0c1c592f21a469
subroh0508 <subroh.0508@gmail.com>
2020-06-21 20:33:23 +0900

    [edit]Arrange Twitter and GitHub link side by side

--- src/main/kotlin/components/organisms/NameCardLinks.kt
+++ src/main/kotlin/components/organisms/NameCardLinks.kt
@@ -7,13 +7,12 @@ import components.molecules.LinkWithIcon
 import components.molecules.LinkWithIconProps
 import kotlinx.css.*
 import kotlinx.html.DIV
+import kotlinx.html.SPAN
 import react.RBuilder
 import react.dom.WithClassName
+import react.dom.span
 import react.functionalComponent
-import styled.StyledDOMBuilder
-import styled.StyledHandler
-import styled.css
-import styled.styledDiv
+import styled.*
 import utilities.styled
 
 external interface NameCardLinksProps : WithClassName {
@@ -26,30 +25,37 @@ external interface NameCardLinksProps : WithClassName {
 val NameCardLinks = functionalComponent<NameCardLinksProps> { props ->
     StyledLinksRoot {
         StyledLinkWithIcon {
-            attrs.href = "https://suroh0508.net"
+            attrs.href = PROTOCOL_HTTPS + props.homepage
             attrs.display = props.homepage
             attrs.icon = HomeIcon
         }
 
-        StyledLinkWithIcon {
-            attrs.href = "https://twiter.com/subroh_0508"
-            attrs.display = props.twitter
-            attrs.icon = TwitterIcon
+        StyledRowSpan {
+            StyledLinkWithIcon {
+                attrs.href = "$PROTOCOL_HTTPS$DOMAIN_TWITTER/${props.twitter}"
+                attrs.display = props.twitter
+                attrs.icon = TwitterIcon
+            }
+
+            StyledLinkWithIcon {
+                attrs.href = "$PROTOCOL_HTTPS$DOMAIN_GITHUB/${props.github}"
+                attrs.display = props.github
+                attrs.icon = GitHubIcon
+            }
         }
 
         StyledLinkWithIcon {
-            attrs.href = "https://github.com/subroh0508"
-            attrs.display = props.github
-            attrs.icon = GitHubIcon
-        }
-
-        StyledLinkWithIcon {
-            attrs.href = "mailto://${props.email}"
+            attrs.href = PROTOCOL_MAIL + props.email
             attrs.display = props.email
             attrs.icon = EmailIcon
         }
     }
 }
+
+private const val PROTOCOL_HTTPS = "https://"
+private const val PROTOCOL_MAIL = "mailto://"
+private const val DOMAIN_TWITTER = "twiter.com"
+private const val DOMAIN_GITHUB = "github.com"
 
 private fun RBuilder.StyledLinksRoot(handler: StyledDOMBuilder<DIV>.() -> Unit) = styledDiv {
     css {
@@ -59,9 +65,21 @@ private fun RBuilder.StyledLinksRoot(handler: StyledDOMBuilder<DIV>.() -> Unit) 
     handler()
 }
 
-private fun RBuilder.StyledLinkWithIcon(handler: StyledHandler<LinkWithIconProps>) = (styled(LinkWithIcon)) {
+private fun RBuilder.StyledRowSpan(handler: StyledDOMBuilder<SPAN>.() -> Unit) = styledSpan {
     css {
         display = Display.flex
+
+        children(a.tagName) {
+            marginRight = 12.px
+        }
+    }
+
+    handler()
+}
+
+private fun RBuilder.StyledLinkWithIcon(handler: StyledHandler<LinkWithIconProps>) = (styled(LinkWithIcon)) {
+    css {
+        marginRight = LinearDimension.auto
     }
 
     handler()
