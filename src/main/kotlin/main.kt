7eab0a8667bfc60f8ab6eb3f6af2260167cdfea3
subroh0508 <subroh.0508@gmail.com>
2020-06-24 22:51:54 +0900

    [edit]Add height: 100% to global styles

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 @file:Suppress("FunctionName")
 
+import components.templates.AppFrame
 import components.templates.IntroductionNameCard
 import react.dom.render
 import kotlin.browser.document
@@ -10,14 +11,25 @@ import themes.ThemeProvider
 
 fun main() {
     injectGlobal {
+        html {
+            height = 100.pct
+            margin(0.px)
+        }
         body {
+            height = 100.pct
+            margin(0.px)
             backgroundColor = Color("#121212")
+        }
+        "div#root" {
+            height = 100.pct
         }
     }
 
     render(document.getElementById("root")) {
         ThemeProvider {
-            child(IntroductionNameCard) {}
+            child(AppFrame) {
+                child(IntroductionNameCard) {}
+            }
         }
     }
 }
