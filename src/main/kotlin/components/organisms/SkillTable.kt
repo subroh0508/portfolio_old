2b3aa36727b04bc098e72a2b54d53ad93e8db8f3
subroh0508 <subroh.0508@gmail.com>
2020-07-25 16:16:43 +0900

    [add]Add SkillTable component to organisms

---
+++ src/main/kotlin/components/organisms/SkillTable.kt
@@ -0,0 +1,72 @@
+@file:Suppress("FunctionName")
+
+package components.organisms
+
+import components.atoms.Paragraph
+import components.atoms.TableRow
+import kotlinext.js.jsObject
+import kotlinx.css.*
+import kotlinx.css.p as cssP
+import kotlinx.css.td as cssTd
+import kotlinx.html.TABLE
+import materialcomponents.VAR_COLOR_TEXT_PRIMARY_ON_DARK
+import react.*
+import react.dom.*
+import styled.css
+import styled.styledTable
+import styled.styledTd
+
+external interface SkillRowProps : WithClassName {
+    var name: String
+    var stars: Int
+    var since: String
+}
+
+fun RBuilder.SkillTable(vararg handler: RHandler<SkillRowProps>) = StyledTable {
+    tbody { handler.forEach { h -> SkillRow(h) } }
+}
+
+@Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
+private fun RBuilder.SkillRow(handler: RHandler<SkillRowProps>) {
+    val builder = SkillTableElementBuilder().apply(handler)
+    val attrs = builder.attrs
+
+    StyledTableRow {
+        td { +attrs.name }
+        td { +((0..4).joinToString("") { i -> if (i < attrs.stars) "★" else "☆" }) }
+        td { +"${attrs.since}-" }
+    }
+
+    StyledTableRowParagraph { childList.addAll(builder.childList) }
+}
+
+private class SkillTableElementBuilder : RElementBuilder<SkillRowProps>(jsObject { })
+
+private fun RBuilder.StyledTable(handler: RDOMBuilder<TABLE>.() -> Unit) = styledTable {
+    css { borderSpacing = 0.px }
+
+    handler()
+}
+
+private fun RBuilder.StyledTableRow(handler: RBuilder.() -> Unit) = TableRow {
+    css {
+        color = Color(VAR_COLOR_TEXT_PRIMARY_ON_DARK.toCustomProperty())
+
+        cssTd {
+            paddingTop = 8.px
+        }
+    }
+
+    handler()
+}
+
+private fun RBuilder.StyledTableRowParagraph(handler: RBuilder.() -> Unit) = TableRow {
+    styledTd {
+        css {
+            cssP { margin(8.px, 0.px) }
+        }
+        attrs { colSpan = "3" }
+
+        child(Paragraph) { handler() }
+    }
+}
