e55f8c558ffcf99a5b79271526083423947616b9
subroh0508 <subroh.0508@gmail.com>
2020-07-25 16:28:18 +0900

    [edit]Change skills to table tag

--- src/main/kotlin/components/templates/SkillCard.kt
+++ src/main/kotlin/components/templates/SkillCard.kt
@@ -4,17 +4,12 @@ package components.templates
 
 import components.atoms.LargeTitle
 import components.atoms.Paragraph
-import components.atoms.Subtitle
 import components.organisms.CardFrame
-import kotlinx.css.*
+import components.organisms.SkillTable
 import react.RBuilder
 import react.child
 import react.dom.WithClassName
-import react.dom.li
-import react.dom.ul
 import react.functionalComponent
-import styled.css
-import styled.styledDiv
 
 val SkillCard = functionalComponent<WithClassName> {
     child(CardFrame) {
@@ -22,43 +17,35 @@ val SkillCard = functionalComponent<WithClassName> {
             attrs.title = "Skill"
         }
 
-        styledDiv {
-            css {
-                display = Display.flex
-
-                h4 { margin(vertical = 8.px) }
-
-            }
-
-            Kotlin()
-            RubyOnRails()
-        }
+        MainSkills()
+        OtherSkills()
     }
 }
 
-private fun RBuilder.Kotlin() = styledDiv {
-    css { width = 50.pct }
+private fun RBuilder.MainSkills() = SkillTable({
+    attrs.name = "Kotlin"
+    attrs.stars = 5
+    attrs.since = "2016.02"
 
-    child(Subtitle) { attrs.subtitle = "Kotlin" }
+    +"v1.0が出る直前に業務で触れて以来、Androidアプリの開発で使い続けています。"
+    +"プライベートでは、Kotlin/JSを使ったWebアプリの開発や、OSS活動にも積極的に関わっています。"
+}, {
+    attrs.name = "Ruby"
+    attrs.stars = 3
+    attrs.since = "2016.06"
 
-    child(Paragraph) {
-        ul {
-            li { +"経験年数: 2016.02-" }
-            li { +"v1.0が出る直前に業務で触れて以来、Androidアプリの開発で使い続けています。" }
-            li { +"プライベートでは、Kotlin/JSを使ったWebアプリの開発や、OSS活動にも積極的に関わっています。" }
-        }
-    }
-}
+    +"業務でRuby on Railsを使い、タスクをこなしています。"
+    +"大規模な機能追加・新規サービスの立ち上げ・DDD + Clean Architectureに沿ったリファクタの経験があります。"
+}, {
+    attrs.name = "Javascript(ES6)"
+    attrs.stars = 3
+    attrs.since = "2016.06"
 
-private fun RBuilder.RubyOnRails() = styledDiv {
-    css { width = 50.pct }
+    +"業務でReactとReduxを使い、タスクをこなしています。"
+    +"大規模な機能追加・新規サービスの立ち上げの経験があります。"
+    +"また、Vue.jsを使って技術ブログを自作した経験もあります。"
+})
 
-    child(Subtitle) { attrs.subtitle = "Ruby on Rails" }
-
-    child(Paragraph) {
-        ul {
-            li { +"経験年数: 2016.06-" }
-            li { +"大規模な機能追加・新規サービスの立ち上げ・DDD + Clean Architectureに沿ったリファクタの経験があります。" }
-        }
-    }
+private fun RBuilder.OtherSkills() = child(Paragraph) {
+    +"その他: Java/C/TypeScript/PostgreSQL/Dart(Flutter)"
 }
