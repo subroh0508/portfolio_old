af7a81157fa7d10c99214359b6f15974b7204702
subroh0508 <subroh.0508@gmail.com>
2022-04-16 01:41:20 +0900

    Enable MPP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
+
 plugins {
-    kotlin("js")
+    kotlin("multiplatform")
     `node-conventions`
 }
 
@@ -28,32 +30,41 @@ kotlin {
             }
         }
     }
+
+    sourceSets {
+        named("jsMain") {
+            dependencies {
+                implementation(enforcedPlatform(Libraries.Kotlin.wrappersBom))
+                implementation(Libraries.Kotlin.react)
+                implementation(Libraries.Kotlin.reactDom)
+                implementation(Libraries.Kotlin.reactRouterDom)
+                implementation(Libraries.Kotlin.history)
+                implementation(Libraries.Kotlin.extensions)
+                implementation(Libraries.Kotlin.emotion)
+
+                implementation(npm("@rmwc/avatar", Libraries.Npm.materialWebComponents))
+                implementation(npm("@rmwc/card", Libraries.Npm.materialWebComponents))
+                implementation(npm("@rmwc/drawer", Libraries.Npm.materialWebComponents))
+                implementation(npm("@rmwc/icon", Libraries.Npm.materialWebComponents))
+                implementation(npm("@rmwc/list", Libraries.Npm.materialWebComponents))
+                implementation(npm("@rmwc/ripple", Libraries.Npm.materialWebComponents))
+                implementation(npm("@rmwc/typography", Libraries.Npm.materialWebComponents))
+                implementation(npm("@rmwc/theme", Libraries.Npm.materialWebComponents))
+
+                implementation(devNpm("style-loader", "*"))
+                implementation(devNpm("html-webpack-plugin", "^5.5.0"))
+                implementation(devNpm("webpack-cdn-plugin", "^3.3.1"))
+            }
+        }
+        named("jsTest") {
+
+        }
+    }
 }
 
-dependencies {
-    implementation(enforcedPlatform(Libraries.Kotlin.wrappersBom))
-    implementation(Libraries.Kotlin.react)
-    implementation(Libraries.Kotlin.reactDom)
-    implementation(Libraries.Kotlin.reactRouterDom)
-    implementation(Libraries.Kotlin.history)
-    implementation(Libraries.Kotlin.extensions)
-    implementation(Libraries.Kotlin.emotion)
+fun KotlinDependencyHandler.enforcedPlatform(dependencyNotation: Any) = project.dependencies.enforcedPlatform(dependencyNotation)
 
-    implementation(npm("@rmwc/avatar", Libraries.Npm.materialWebComponents))
-    implementation(npm("@rmwc/card", Libraries.Npm.materialWebComponents))
-    implementation(npm("@rmwc/drawer", Libraries.Npm.materialWebComponents))
-    implementation(npm("@rmwc/icon", Libraries.Npm.materialWebComponents))
-    implementation(npm("@rmwc/list", Libraries.Npm.materialWebComponents))
-    implementation(npm("@rmwc/ripple", Libraries.Npm.materialWebComponents))
-    implementation(npm("@rmwc/typography", Libraries.Npm.materialWebComponents))
-    implementation(npm("@rmwc/theme", Libraries.Npm.materialWebComponents))
-
-    implementation(devNpm("style-loader", "*"))
-    implementation(devNpm("html-webpack-plugin", "^5.5.0"))
-    implementation(devNpm("webpack-cdn-plugin", "^3.3.1"))
-}
-
-val browserWebpack = tasks.getByName("browserProductionWebpack")
+val jsBrowserWebpack = tasks.getByName("jsBrowserProductionWebpack")
 
 val copyDistributions by tasks.registering {
     doLast {
@@ -69,4 +80,4 @@ val copyDistributions by tasks.registering {
     }
 }
 
-browserWebpack.finalizedBy(copyDistributions)
+jsBrowserWebpack.finalizedBy(copyDistributions)
