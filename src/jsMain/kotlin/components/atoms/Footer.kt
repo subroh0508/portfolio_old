25ee23ce3a8aa1f2b460219a408cacd1b98e001a
subroh0508 <subroh.0508@gmail.com>
2022-04-17 19:44:06 +0900

    Replace Footer to Compose

--- src/jsMain/kotlin/components/atoms/Footer.kt
+++ src/jsMain/kotlin/components/atoms/Footer.kt
@@ -2,33 +2,52 @@
 
 package components.atoms
 
-import csstype.*
-import emotion.react.css
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Br
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Span
+import org.jetbrains.compose.web.dom.Text
+import org.w3c.dom.HTMLElement
 import react.VFC
-import react.dom.html.ReactHTML.br
 import react.dom.html.ReactHTML.div
-import react.dom.html.ReactHTML.span
+import react.useRef
+import utilities.ref
+import utilities.useCompose
 
 val Footer = VFC {
+    val containerRef = useRef<HTMLElement>(null)
+
+    useCompose(containerRef) {
+        ComposableFooter()
+    }
+
     div {
-        css {
-            display = Display.flex
-            height = 100.vh - (800 * 55.0 / 91.0).px - 64.px
-            minHeight = 128.px
-            alignItems = AlignItems.flexEnd
-            color = rgba(255, 255, 255, 0.38)
+        ref { containerRef.current = it }
+    }
+}
+
+@Composable
+fun ComposableFooter() {
+    Div({
+        style {
+            display(DisplayStyle.Flex)
+            height(100.vh - (800 * 55.0 / 91.0).px - 64.px)
+            minHeight(128.px)
+            alignItems(AlignItems.FlexEnd)
+            color(rgba(255, 255, 255, 0.38))
         }
-
-        span {
-            css {
-                width = 100.pct
-                textAlign = TextAlign.center
-                padding = Padding(40.px, 0.px)
+    }) {
+        Span({
+            style {
+                width(100.percent)
+                textAlign("center")
+                padding(40.px, 0.px)
             }
-
-            +"Last Modified: 2022-04-13T01:34:09+0900"
-            br {}
-            +"Copyright© 2022 subroh_0508 Official Website. All Rights Reserved."
+        }) {
+            Text("Last Modified: 2022-04-13T01:34:09+0900")
+            Br {}
+            Text("Copyright© 2022 subroh_0508 Official Website. All Rights Reserved.")
         }
     }
 }
